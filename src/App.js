8653a906255b48650427213c40c56d1623f2488d
Rajan G <rajangrg1@hotmail.com>
2020-01-30 23:57:00 +1300

    added- card and search component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Form } from 'semantic-ui-react';
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 import './App.css';
 //components
@@ -6,10 +8,33 @@ import Header from './component/header/header.component';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header>Github User Finder</Header>
 
+      <div className="search">
+        <Form>
+          <Form.Group>
+            <Form.Input placeholder="Github User" name="name" />
+            <Form.Button content="Search" />
+          </Form.Group>
+        </Form>
+      </div>
+
+      <Card>
+    <Image src='/matthew.png' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+
+  </Card>
     </div>
+
   );
 }
 
